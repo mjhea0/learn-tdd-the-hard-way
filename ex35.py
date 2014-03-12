a852f6631fe5dfd0c8aef279d617814fd78c3e10
Michael Herman <hermanmu@gmail.com>
2014-03-12 09:53:16 -0600

    updates

--- ex35.py
+++ ex35.py
@@ -1,76 +1,91 @@
+import os
+
 
 class BranchesFunctions(object):
 
-    def __init__(self, which_room, timid_bear):
-        self.which_room = which_room
+    def __init__(self, timid_bear):
         self.timid_bear = timid_bear
 
     def gold_room(self):
-        next = raw_input("\nThis room is full of gold. How much do you take? (Round to the nearest 10) ")
+        next = raw_input("\nThis room is full of gold. \nHow much do you take? (round to the nearest 10) ")
 
         if "0" in next:
             how_much = int(next)
         else:
-            return "Man, learn to type a number."
-
+            return "\nMan, round to the neartest 10.\n"
         if how_much < 50:
-            return "Nice, you're not greedy, you win!"
+            return "\nNice, you're not greedy, you win!\n"
         else:
-            return "You greedy bastard!"
+            return "\nYou greedy bastard!\n"
 
     def bear_room(self):
         while True:
-            next = raw_input("Do you 'take honey' or 'taunt bear'? ")
+            next = raw_input("\nDo you take the 'honey' or 'taunt' the bear? ")
             output_list = []
 
-            if next == "take honey":
-                output_list.append("\nThe bear looks at you then slaps your face off.")
-            elif next == "taunt bear" and not self.timid_bear:
+            if next == "honey":
+                return "\nThe bear looks at you then slaps your face off.\n"
+            elif next == "taunt" and not self.timid_bear:
                 output = "\nThe bear has moved from the door. You can go through it now."
                 self.timid_bear = True
                 open_door = True
                 output_list.extend([output,open_door])
-            elif next == "taunt bear" and self.timid_bear:
-                output_list.append("\nThe bear gets pissed off and chews your leg off.")
+                return output_list
+            elif next == "taunt" and self.timid_bear:
+                return "\nThe bear gets pissed off and chews your leg off.\n"
             else:
-                output_list.append("\nI got no idea what that means. Bye.")
-            return output_list
+                return "\nI got no idea what that means. Bye.\n"
 
     def cthulhu_room(self):
-        start_over = False
+        start_over = True
         next_list = []
-        next = raw_input("Do you 'flee' for your life or eat your 'head'? ")
+        next = raw_input("\nDo you 'flee' for your life or eat your 'head'? ")
 
-        if "flee" in next:
-            start_over = True
-            next_list.append(start_over)
-        elif "head" in next:
-            next_list.extend([0,"Well that was tasty!"])
+        if "head" in next:
+            return "Well that was tasty!"
         else:
-            next_list.append(start_over)
-        return next_list
+            return start_over
+
+    def start(self):
+        which_room = raw_input("There is a door to your 'right' and 'left'. \nWhich one do you take? ")
+        return which_room
+
+    def cls(self):
+        os.system(['clear','cls'][os.name == 'nt'])
 
 
 if __name__ == '__main__':
-    which_room = raw_input("\nThere is a door to your 'right' and 'left'. Which one do you take? ")
+    
     timid_bear = False
-    create = BranchesFunctions(which_room, timid_bear)
+    create = BranchesFunctions(timid_bear)
+    create.cls()
+    print"#---------START!---------#"
+    which_room = create.start()
+
+    # bear room
     if which_room == "left":
-        print "\nThere is a bear here. The bear has a bunch of honey. \
-The fat bear is in front of another door. How are you going to move the bear?"
+        create.cls()
+        print"#---------BEAR ROOM---------#"
+        print "There is a bear here.\nThe bear has a bunch of honey. \
+\nThe fat bear is in front of another door. \nHow are you going to move the bear?"
         results = create.bear_room()
-        print results[0]
-        if results[1]:
+        if type(results) == list:
+            create.cls()
+            print"#---------GOLD ROOM---------#"
+            # gold room
             print create.gold_room()
+        else: 
+            print results
+    # cthulhu room
     elif which_room == "right":
-        print "Here you see the great evil Cthulhu. He, it, whatever stares at you and you go insane."
+        create.cls()
+        print"#---------CTHULHU ROOM---------#"
+        print "Here you see the great evil Cthulhu. \nHe, it, whatever stares at you and you go insane."
         results = create.cthulhu_room()
-        if results[0] == False:
-            results = create.cthulhu_room()
-        elif results[0] == True:
-            which_room = raw_input("\nThere is a door to your 'right' and 'left'. Which one do you take? ")
+        if results == True:
+            # results = create.cthulhu_room()
         else:
-            print results[1]
-    else:
-        print "You stumble around in the dark."
+            # which_room = create.start()
+#     else:
+#         print "You stumble around in the dark."
     
